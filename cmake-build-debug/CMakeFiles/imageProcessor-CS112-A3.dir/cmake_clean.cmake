a9a235fe98a90ee256e55d2b28a0a7af701afb8e
Loai Hataba <LoaiWleed2005@hotmail.com>
2024-04-16 15:29:44 +0200

    Adding more buttons for filters

---
+++ cmake-build-debug/CMakeFiles/imageProcessor-CS112-A3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageProcessor-CS112-A3.dir/CS112_A3_Part1_S24_20230553_20230231_20230121.cpp.obj"
+  "CMakeFiles/imageProcessor-CS112-A3.dir/CS112_A3_Part1_S24_20230553_20230231_20230121.cpp.obj.d"
+  "CMakeFiles/imageProcessor-CS112-A3.dir/ImageProcessingDemo.cpp.obj"
+  "CMakeFiles/imageProcessor-CS112-A3.dir/ImageProcessingDemo.cpp.obj.d"
+  "imageProcessor-CS112-A3.exe"
+  "imageProcessor-CS112-A3.exe.manifest"
+  "imageProcessor-CS112-A3.pdb"
+  "libimageProcessor-CS112-A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageProcessor-CS112-A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
